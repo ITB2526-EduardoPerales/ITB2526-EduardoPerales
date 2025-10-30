ef9fbe5a9f08fd458c592de0d36b1bc37c475b6c
eduardo.perales.7e7 <eduardo.perales.7e7@itb.cat>
2025-10-30 09:50:32 +0100

    T04

---
+++ TA04/password.py
@@ -0,0 +1,9 @@
+PWD = "asdasd"
+clave = input("Dime la clave:")
+intento = 0
+
+while clave != PWD and intento < 2:
+    print("Clave incorrecta!!!")
+    intento = intento + 1
+    clave = input("Dime la clave:")
+print("Programa terminado")
